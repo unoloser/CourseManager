b9400e839e17ca687c67f48639092d01087fc03d
JQLu <lujiaqi19940905@gmail.com>
2020-06-07 19:45:05 -0400

    Bug fixed
    Need to add links and visibility

--- initiateInfo.js
+++ initiateInfo.js
@@ -1,20 +1,29 @@
 window.onload = function () {
-    chrome.storage.sync.get(["name", "studentID", "CIT591", "CIT592",
+    var proctorULink = "https://www.proctoru.com/"
+    var proUBtn = document.getElementById("goToExam")
+    proUBtn.addEventListener('click', function () {
+        window.open(proctorULink);
+    })
+
+
+    chrome.storage.sync.get(["studentName", "studentID", "CIT591", "CIT592",
         "CIT593", "CIT594", "CIT595", "CIT596"], function (items) {
             if (!chrome.runtime.error) {
-                if (!items.name == undefined) {
+                if (items.studentName != "undefined") {
                     var greetings = document.getElementById("greet1");
-                    greetings.innerHTML = "Hi! " + items.name;
+                    greetings.innerText = "Hi! " + items.studentName;
                 }
-                if (!items.studentID == undefined) {
+                if (items.studentID != "undefined") {
                     var greetings = document.getElementById("greet2");
-                    greetings.innerHTML = "Student ID: " + items.studentID;
+                    greetings.innerText = "Student ID: " + items.studentID;
                 }
+
+
 
 
             } else {
                 console.log("Error loading info from Chrome")
             }
         }
-    );
+    )
 }
--- saveInfo.js
+++ saveInfo.js
@@ -1,6 +1,7 @@
+// chrome.storage.sync.get(function(result){console.log(result)})
 var saveBtn = document.getElementById('save');
 
-var name = document.getElementById("updatedName");
+var studentName = document.getElementById("updatedName");
 var studentID = document.getElementById("updatedID");
 var CIT591 = document.getElementById("CIT591");
 var CIT592 = document.getElementById("CIT592");
@@ -12,16 +13,17 @@ var CIT596 = document.getElementById("CIT596");
 if (saveBtn) {
     saveBtn.addEventListener('click', function () {
         // document.getElementById('stdout').innerText = 'loaded'
-        name = name.value;
+        studentName = studentName.value;
         studentID = studentID.value;
-        CIT591 = CIT591.value;
-        CIT592 = CIT592.value;
-        CIT593 = CIT593.value;
-        CIT594 = CIT594.value;
-        CIT595 = CIT595.value;
-        CIT596 = CIT596.value;
+        // document.getElementById('stdout').innerText = studentName;
+        CIT591 = CIT591.checked;
+        CIT592 = CIT592.checked;
+        CIT593 = CIT593.checked;
+        CIT594 = CIT594.checked;
+        CIT595 = CIT595.checked;
+        CIT596 = CIT596.checked;
         chrome.storage.sync.set({
-            "name": name,
+            "studentName": studentName,
             "studentID": studentID,
             "CIT591": CIT591,
             "CIT592": CIT592,
@@ -32,47 +34,21 @@ if (saveBtn) {
         }, function () {
             if (chrome.runtime.error) {
                 console.log("Runtime error.");
-                document.getElementById('stdout').innerText = 'Save failed';
-            }
-            else {
-                document.getElementById('stdout').innerText = 'Save okay';
             }
         });
 
-        window.close();
+        // window.close();
     });
 }
 
-// document.body.onload = function () {
-
-//     chrome.storage.sync.get(["name", "studentID", "CIT591", "CIT592",
-//         "CIT593", "CIT594", "CIT595", "CIT596"], function (items) {
-//             if (!chrome.runtime.error) {
-//                 console.log(items);
-
-//                 // load user identity
-//                 name.value = items.name == null ? "" : items.name;
-//                 studentID.value = items.studentID == null ? "" : items.studentID;
-//                 // load courses
-//                 CIT591.checked = items.CIT591;
-//                 CIT592.checked = items.CIT592;
-//                 CIT593.checked = items.CIT593;
-//                 CIT594.checked = items.CIT594;
-//                 CIT595.checked = items.CIT595;
-//                 CIT596.checked = items.CIT596;
-//             }
-//         });
-// }
 
 window.onload = function () {
 
-    chrome.storage.sync.get(["name", "studentID", "CIT591", "CIT592",
+    chrome.storage.sync.get(["studentName", "studentID", "CIT591", "CIT592",
         "CIT593", "CIT594", "CIT595", "CIT596"], function (items) {
             if (!chrome.runtime.error) {
-                console.log(items);
-
                 // load user identity
-                name.value = items.name == null ? "" : items.name;
+                studentName.value = items.studentName == null ? "" : items.studentName;
                 studentID.value = items.studentID == null ? "" : items.studentID;
                 // load courses
                 CIT591.checked = items.CIT591;
